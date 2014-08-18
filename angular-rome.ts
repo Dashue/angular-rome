3f1b091fd6bf9ed95dced8fd51564cb0d858efc2
Johan Nilsson <jonh04@gmail.com>
2014-08-17 21:33:59 -0400

    Added Typescript version

---
+++ angular-rome.ts
@@ -0,0 +1,34 @@
+﻿
+var romeDirective = () => {
+
+    var directive :ng.IDirective = {} ;
+
+    directive.require = '^ngModel';
+
+    directive.restrict = 'EA';
+
+    directive.scope = {
+        options: '=',
+        ngModel: '=',
+    };
+
+    directive.link = (scope : IRomeScope, iElement, iAttrs, ctrl, transclude) => {
+        var cal = rome(iElement[0], scope.options);
+
+        cal.on('data', value => {
+            scope.ngModel = value;
+            scope.$apply();
+        });
+
+    }
+
+    return directive;
+}
+
+interface IRomeScope extends ng.IScope {
+    options;
+    ngModel;
+}
+
+angular.module('rome', [])
+    .directive('datePicker', romeDirective);
